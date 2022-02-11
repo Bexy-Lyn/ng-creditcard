b365266ce8adfa62155947efee9e9de946075332
Rebecca Otto <rebecca.otto@eyeworkers.de>
2022-02-11 11:25:16 +0100

    input masking and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { IConfig, NgxMaskModule } from 'ngx-mask';
 
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
+import { CardComponent } from './card/card.component';
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 
 @NgModule({
-  declarations: [AppComponent, CardFormComponent, InputComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  declarations: [AppComponent, CardFormComponent, InputComponent, CardComponent],
+  imports: [BrowserModule, ReactiveFormsModule, NgxMaskModule.forRoot(options)],
   providers: [],
   bootstrap: [AppComponent],
 })
